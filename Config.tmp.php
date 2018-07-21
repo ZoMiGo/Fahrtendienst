5301673f017f126db0ae54a17a3be49a39d12814
Goran <gorance@live.de>
2018-07-21 09:01:09 +0200

    Add files via upload

---
+++ Config.tmp.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by Trajilovic Goran.
+ * Email : gorance@live.de
+ * Date: 19/02/2017
+ * Time: 12:32
+ */
+/*
+ * All database connection variables
+ */
+
+    define('DB_SERVER', ":SERVER_NAME:");// db server
+    define('DB_USER', ":USER_NAME:");// db user
+    define('DB_PASSWORD', ":USER_PASSWORD:");// db password (mention your db password here)
+    define('DB_DATABASE', ":DB_NAME:");// database name
+    define('DB_TABLE_PREFIX', ":DB_TABLE_PREFIX:");//database prefix
+    define('DB_DRIVER', "mysql"); // db server
+
+$basetyp = "mysql";
+$host = ":SERVER_NAME:";// db server
+$db_name = ":DB_NAME:";// database name
+$db_username = ":USER_NAME:";// db user
+$db_password = ":USER_PASSWORD:";// db password (mention your db password here)
+?>
